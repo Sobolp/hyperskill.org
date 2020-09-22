97c3e73b465e796329014dd09835ab0b6af85d10
SoBoLp <pbolotov@gmail.com>
2020-09-22 14:04:04 +0200

    Creating instances -> Constructor Checker https://hyperskill.org/learn/step/2879

---
+++ practice/src/ConstructorChecker.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+/**
+ * Implement checkPublicParameterlessConstructor that returns true if the provided class
+ * declares public parameterless constructor and false otherwise.
+ */
+
+/**
+ Check whether the class declares public parameterless constructor
+ */
+class ConstructorChecker {
+
+    public boolean checkPublicParameterlessConstructor(Class<?> clazz) {
+        // Add implementation here
+        return Arrays.stream(clazz.getConstructors()).filter(x -> x.getParameterCount() == 0).count() > 0;
+    }
+
+}
