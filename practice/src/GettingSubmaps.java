4274422e30f0c2dbc0576e539903b962c159103e
SoBoLp <pbolotov@gmail.com>
2020-10-24 13:08:59 +0200

    Map -> Getting submaps https://hyperskill.org/learn/step/2783

---
+++ practice/src/GettingSubmaps.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Write a program that reads key-value pairs and outputs pairs whose keys belong to the specified range (inclusive)
+ * in the ascending order by the key.
+ * <p>
+ * Input data format
+ * <p>
+ * The first line contains two values that represent range borders.
+ * The second line contains the number of key-value pairs.
+ * Next lines contain pair (an integer key and a string value separated by a space).
+ * <p>
+ * Output data format
+ * <p>
+ * All pairs whose keys belong to the range, each pair in a new line.
+ * The key and the value of a pair must be separated by a space.
+ * <p>
+ * Report a typo
+ * Sample Input 1:
+ * 2 4
+ * 5
+ * 1 aa
+ * 5 ee
+ * 2 bb
+ * 4 dd
+ * 3 cc
+ * Sample Output 1:
+ * 2 bb
+ * 3 cc
+ * 4 dd
+ */
+public class GettingSubmaps {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int fromKey = scanner.nextInt();
+        final int toKey = scanner.nextInt();
+        final int n = scanner.nextInt();
+        scanner.nextLine();
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] entry = scanner.nextLine().split("\\s+");
+            map.put(Integer.parseInt(entry[0]), entry[1]);
+        }
+        for (int key : map.keySet()) {
+            if (key >= fromKey && key <= toKey) {
+                System.out.println(key + " " + map.get(key));
+            }
+        }
+    }
+}
