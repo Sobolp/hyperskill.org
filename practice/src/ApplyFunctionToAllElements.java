2d2d56225953aafa20e0050b334880535c1f8f56
SoBoLp <pbolotov@gmail.com>
2020-10-25 14:58:04 +0100

    Method references -> Apply function to all elements https://hyperskill.org/learn/step/4330

---
+++ practice/src/ApplyFunctionToAllElements.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+/**
+ * You need to implement a method that applies given function to all elements of the given array.
+ * You need to change the existing array, and not create a new array.
+ */
+public class ApplyFunctionToAllElements {
+    public static <T> void applyFunction(T[] array, Function<T, T> func) {
+        // write your code here
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = func.apply(array[i]);
+//        }
+        IntStream.range(0, array.length).forEach(i -> array[i] = func.apply(array[i]));
+    }
+
+    // do not change code below
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String method = scanner.nextLine();
+        String[] array = scanner.nextLine().split(" ");
+        applyFunction(array,
+                "lower".equals(method) ? String::toLowerCase :
+                        "upper".equals(method) ? String::toUpperCase :
+                                "new".equals(method) ? String::new :
+                                        "trim".equals(method) ? String::trim : String::intern);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+}
